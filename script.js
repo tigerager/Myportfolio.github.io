c7782ae7159ba80d8410eb3d00e94ca7c1dde1bd
tigerager <50732543+tigerager@users.noreply.github.com>
2023-09-08 19:10:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function addRecommendation() {
+  // Get the message of the new recommendation
+  let recommendation = document.getElementById("new_recommendation");
+  // If the user has left a recommendation, display a pop-up
+  if (recommendation.value != null && recommendation.value.trim() != "") {
+
+    showPopup(true);
+    // Create a new 'recommendation' element and set it's value to the user's message
+    var element = document.createElement("div");
+    element.setAttribute("class","records");
+    element.innerHTML = "<span><q>" + recommendation.value + "</q></span>";
+    // Add this element to the end of the list of recommendations
+    document.getElementById("recomWr").appendChild(element); 
+    
+    // Reset the value of the textarea
+    recommendation.value = "";
+  }
+}
+
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
+}
